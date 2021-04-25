7df3fb368d0df0d814839bb983048d35c2ebcb4f
Timothy Nunnink <timothyjnunnink@gmail.com>
2021-04-24 22:28:44 -0500

    updated some packages mostly in attempt to load project onto personal pc

--- tests/TestConsole/Program.cs
+++ tests/TestConsole/Program.cs
@@ -8,18 +8,7 @@ namespace TestConsole
     {
         private static void Main(string[] args)
         {
-            /*var service = new GalaxyRegistry();
-            service.RegisterAll();
-            Console.WriteLine("Press and Key to stop");
-            Console.ReadLine();*/
 
-            /*var service = new GalaxyArchiveService(new GalaxyRegistry());
-            service.Start();
-
-            Console.WriteLine("Press Key to stop");
-            Console.ReadLine();
-
-            service.Stop();*/
         }
     }
 }
